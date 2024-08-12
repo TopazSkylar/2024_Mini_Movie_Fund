a6a40bf71289a30cf43f96e79d116b3284c51568
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2024-08-12 14:56:50 +1200

    Age checker w num checker

---
+++ 04_age_checker.py
@@ -0,0 +1,36 @@
+
+
+# function
+def num_check(question):
+
+    while True:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print("Please enter an integer")
+
+
+tickets_sold = 0
+
+while True:
+
+    name + input("Enter your name / xxx to quit: ")
+
+    if name == "xxx":
+        break
+
+    age = num_checker("Age: ")
+
+    if 12 <= age <= 120:
+        pass
+    elif age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    else:
+        print("?? That looks like a typo, please try again")
+        continue
+
+    tickets_sold += 1
