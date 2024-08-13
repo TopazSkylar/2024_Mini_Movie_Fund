7d2473b3ed99a173e46b026b3c1d7da7a9333722
SwathedSplash62 <138179868+SwathedSplash62@users.noreply.github.com>
2024-08-14 09:56:25 +1200

    Instruct, name, age, price, payment

--- 00_MM_Base_v01.py
+++ 00_MM_Base_v01.py
@@ -1,21 +1,5 @@
 # functions go here
 
-# checks for yes and no answers to questions
-def yes_no(question):
-    while True:
-        response = input(question).lower()
-        # checks user response to question
-        # only accepts yes or no
-        if response == "yes" or response == "y":
-            return "yes"
-
-        elif response == "no" or response == "n":
-            return "no"
-
-        else:
-            print("Please answer yes or no ")
-
-
 # checks that the user response is not blank
 def not_blank(question):
     while True:
@@ -28,14 +12,65 @@ def not_blank(question):
             return response
 
 
+# checks that the user inputs an integer
+def num_check(question):
+    while True:
+
+        try:
+            response = int(input(question))
+            return response
+
+        except ValueError:
+            print("Please enter an integer")
+
+
+# Calculate the ticket price based on the age
+def calc_ticket_price(var_age):
+    # ticket is $7.50 for users under 16
+    if var_age < 16:
+        price = 7.5
+
+    # ticket is $10.50 for users between 16 and 64
+    elif var_age < 65:
+        price = 10.5
+
+    # ticket price is $6.50 for seniors (65+)
+    else:
+        price = 6.5
+
+    return price
+
+
+# checks that users enter a valid response (yes/no, cash/credit) based on a list of options
+def string_checker(question, num_letters, valid_responses):
+    error = "Please choose {} or {}".format(valid_responses[0], valid_responses[1])
+
+    if num_letters == 1:
+        short_version = 1
+    else:
+        short_version = 2
+
+    while True:
+        response = input(question).lower()
+
+        for item in valid_responses:
+            if response == item[:short_version] or response == item:
+                return item
+
+        print(error)
+
+
 # main routine goes here
 
 # set maximum number of tickets below
 MAX_TICKETS = 3
 tickets_sold = 0
 
+yes_no_list = ["yes", "no"]
+payment_list = ["cash", "credit"]
+
 # ask user if they want to see the instructions
-instructions = yes_no("Do you want to read the instructions? ")
+instructions = string_checker("Do you want to read the instructions? ")
 
 if instructions == "yes":
     print("Instructions go here")
@@ -48,10 +83,29 @@ print()
 while tickets_sold < MAX_TICKETS:
     name = not_blank("Please enter your name or 'xxx' to quit : ")
 
-    tickets_sold += 1
-
     if name == 'xxx':
         break
+
+    age = num_check("Age: ")
+
+    if 12 <= age <= 120:
+        pass
+    elif age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    else:
+        print("?? That looks like a typo, please try again")
+        continue
+
+    # calculate ticket cost
+    ticket_cost = calc_ticket_price(age)
+
+    # get payment method
+    for case in range(0, 5):
+        pay_method = string_checker("Choose a payment method (cash/credit)", 2, payment_list)
+        print("You chose", pay_method)
+
+    tickets_sold += 1
 
 # Output number of tickets sold
 if tickets_sold == MAX_TICKETS:
