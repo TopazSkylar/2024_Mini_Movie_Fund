149bcffd7d6373e77b2cc51a555a667aa5b68daf
TopazSkylar <138179868+TopazSkylar@users.noreply.github.com>
2025-02-21 13:26:26 +1300

    Randomly selects a ticket to be free

--- 09_raffle_winner.py
+++ 09_raffle_winner.py
@@ -8,18 +8,18 @@ def currency(x):
 
 
 # dictionaries to hold ticket details
-all_names = ["a", "b", "c", "d", "e"]
+all_names = ["A", "B", "C", "D", "E"]
 all_ticket_costs = [7.50, 7.50, 10.50, 6.50, 6.50]
-surcharge = [0, 0, 0.53, 0.53, 0]
+all_surcharges = [0, 0, 0.53, 0.53, 0]
 
 mini_movie_dict = {
     "Name": all_names,
     "Ticket Price": all_ticket_costs,
-    "Surcharge": surcharge
+    "Surcharge": all_surcharges
 }
 
-mini_movie_frame = pandas.DataFrame(mini_movie_dict)
 # mini_movie_frame = mini_movie_frame.set_index('Name')
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
 
 # Calculate the total ticket cost (ticket + surcharge)
 mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
@@ -28,42 +28,29 @@ mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
 # Calculate the profit for each ticket
 mini_movie_frame['Profit'] = mini_movie_frame['Ticket Price'] - 5
 
+# Calculate ticket and profit totals
+total_paid = mini_movie_frame['Total'].sum()
+total_profit = mini_movie_frame['Profit'].sum()
+
+# print(mini_movie_frame)
+print(mini_movie_frame.to_string(index=False))
+
 # Choose a winner from a name list
 winner_name = random.choice(all_names)
 
-# get position of winner name in list
-win_index = all_names.index(winner_name)
+# find index of winner (ie. position in list)
+winner_index = all_names.index(winner_name)
+print("winner", winner_name, "list position", winner_index)
 
 # look up total amount won (ie: ticket price + surcharge)
-total_won = mini_movie_frame.at[win_index, 'Total']
+winner_ticket_price = all_ticket_costs[winner_index]
+winner_surcharge = all_surcharges[winner_index]
 
-# set index at end (before printing)
-mini_movie_frame = mini_movie_frame.set_index('Name')
-print(mini_movie_frame)
+# total won
+total_won = winner_ticket_price + winner_surcharge
 
 print()
 print('---- Raffle Winner ----')
-print(" Congrats {}. You have won ${} ie: your ticket is free!".format(winner_name, total_won))
+print(f" Congrats {winner_name}. Their ticket of ${total_won:.2f} is free!")
 
 
-# Calculate ticket and profit totals
-total = mini_movie_frame['Total'].sum()
-profit = mini_movie_frame['Profit'].sum()
-
-# Currency formatting (uses currency function)
-add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
-for var_item in add_dollars:
-    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
-
-print("---- Ticket Data ----")
-print()
-
-# output table with ticket data
-print(mini_movie_frame)
-
-print()
-print("----- Ticket Cost / Profit -----")
-
-# output table with profit totals
-print("Total Ticket Sales: ${:.2f}".format(total))
-print("Total Profit : ${:.2f}".format(profit))
